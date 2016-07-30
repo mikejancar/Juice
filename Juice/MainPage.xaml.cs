88a343d3414af4396436998eec8be5c967b51b1e
Michael Jancar <mikejancar@outlook.com>
2016-07-30 10:29:32 -0400

    Added basic battery reporting functionality

--- Juice/MainPage.xaml.cs
+++ Juice/MainPage.xaml.cs
@@ -1,30 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Windows.Devices.Enumeration;
+using Windows.Devices.Power;
+using Windows.UI.Core;
 
 namespace Juice
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        bool reportRequested = false;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Battery.AggregateBattery.ReportUpdated += AggregateBattery_ReportUpdated;
+            RequestAggregateBatteryReport();
+        }
+
+        private void RequestAggregateBatteryReport()
+        {
+            BatteryReportPanel.Children.Clear();
+
+            var aggBattery = Battery.AggregateBattery;
+            var report = aggBattery.GetReport();
+
+            AddReportUI(BatteryReportPanel, report, aggBattery.DeviceId);
+
+            reportRequested = true;
+        }
+
+        private void AddReportUI(StackPanel sp, BatteryReport report, string DeviceID)
+        {
+            TextBlock txt2 = new TextBlock { Text = "Battery status: " + report.Status.ToString() };
+            txt2.FontStyle = Windows.UI.Text.FontStyle.Italic;
+            txt2.Margin = new Thickness(0, 0, 0, 15);
+
+            // Create energy capacity progress bar & labels
+            TextBlock pbLabel = new TextBlock { Text = "Percent remaining energy capacity" };
+            pbLabel.Margin = new Thickness(0, 10, 0, 5);
+            pbLabel.FontFamily = new FontFamily("Segoe UI");
+            pbLabel.FontSize = 11;
+
+            ProgressBar pb = new ProgressBar();
+            pb.Margin = new Thickness(0, 5, 0, 0);
+            pb.Width = 200;
+            pb.Height = 10;
+            pb.IsIndeterminate = false;
+            pb.HorizontalAlignment = HorizontalAlignment.Left;
+
+            TextBlock pbPercent = new TextBlock();
+            pbPercent.Margin = new Thickness(0, 5, 0, 10);
+            pbPercent.FontFamily = new FontFamily("Segoe UI");
+            pbLabel.FontSize = 11;
+
+            // Disable progress bar if values are null
+            if ((report.FullChargeCapacityInMilliwattHours == null) ||
+                (report.RemainingCapacityInMilliwattHours == null))
+            {
+                pb.IsEnabled = false;
+                pbPercent.Text = "N/A";
+            }
+            else
+            {
+                pb.IsEnabled = true;
+                pb.Maximum = Convert.ToDouble(report.FullChargeCapacityInMilliwattHours);
+                pb.Value = Convert.ToDouble(report.RemainingCapacityInMilliwattHours);
+                pbPercent.Text = ((pb.Value / pb.Maximum) * 100).ToString("F2") + "%";
+            }
+
+            // Add controls to stackpanel
+            sp.Children.Add(txt2);
+            sp.Children.Add(pbLabel);
+            sp.Children.Add(pb);
+            sp.Children.Add(pbPercent);
+        }
+
+        async private void AggregateBattery_ReportUpdated(Battery sender, object args)
+        {
+            if (reportRequested)
+            {
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    RequestAggregateBatteryReport();
+                });
+            }
         }
     }
 }
