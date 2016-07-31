621727889ad36393f4f9039a2dd207dfdc31312a
Michael Jancar <mikejancar@outlook.com>
2016-07-31 08:58:05 -0400

    Defined UI in XAML and cleaned up code-behind

--- Juice/MainPage.xaml.cs
+++ Juice/MainPage.xaml.cs
@@ -1,91 +1,73 @@
 ﻿using System;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media;
-using Windows.Devices.Enumeration;
 using Windows.Devices.Power;
 using Windows.UI.Core;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
 
 namespace Juice
 {
     public sealed partial class MainPage : Page
     {
-        bool reportRequested = false;
-
         public MainPage()
         {
             this.InitializeComponent();
-            Battery.AggregateBattery.ReportUpdated += AggregateBattery_ReportUpdated;
             RequestAggregateBatteryReport();
+            Battery.AggregateBattery.ReportUpdated += AggregateBattery_ReportUpdated;
         }
 
         private void RequestAggregateBatteryReport()
         {
-            BatteryReportPanel.Children.Clear();
-
             var aggBattery = Battery.AggregateBattery;
             var report = aggBattery.GetReport();
 
-            AddReportUI(BatteryReportPanel, report, aggBattery.DeviceId);
-
-            reportRequested = true;
+            UpdateReportUI(BatteryReportPanel, report, aggBattery.DeviceId);
         }
 
-        private void AddReportUI(StackPanel sp, BatteryReport report, string DeviceID)
+        private void UpdateReportUI(StackPanel sp, BatteryReport report, string DeviceID)
         {
-            TextBlock txt2 = new TextBlock { Text = "Battery status: " + report.Status.ToString() };
-            txt2.FontStyle = Windows.UI.Text.FontStyle.Italic;
-            txt2.Margin = new Thickness(0, 0, 0, 15);
+            BatteryStatus.Text = string.Format("Status: {0}", report.Status);
 
-            // Create energy capacity progress bar & labels
-            TextBlock pbLabel = new TextBlock { Text = "Percent remaining energy capacity" };
-            pbLabel.Margin = new Thickness(0, 10, 0, 5);
-            pbLabel.FontFamily = new FontFamily("Segoe UI");
-            pbLabel.FontSize = 11;
-
-            ProgressBar pb = new ProgressBar();
-            pb.Margin = new Thickness(0, 5, 0, 0);
-            pb.Width = 200;
-            pb.Height = 10;
-            pb.IsIndeterminate = false;
-            pb.HorizontalAlignment = HorizontalAlignment.Left;
-
-            TextBlock pbPercent = new TextBlock();
-            pbPercent.Margin = new Thickness(0, 5, 0, 10);
-            pbPercent.FontFamily = new FontFamily("Segoe UI");
-            pbLabel.FontSize = 11;
-
-            // Disable progress bar if values are null
             if ((report.FullChargeCapacityInMilliwattHours == null) ||
                 (report.RemainingCapacityInMilliwattHours == null))
             {
-                pb.IsEnabled = false;
-                pbPercent.Text = "N/A";
+                BatteryBar.IsEnabled = false;
+                BatteryCharge.Text = "N/A";
             }
             else
             {
-                pb.IsEnabled = true;
-                pb.Maximum = Convert.ToDouble(report.FullChargeCapacityInMilliwattHours);
-                pb.Value = Convert.ToDouble(report.RemainingCapacityInMilliwattHours);
-                pbPercent.Text = ((pb.Value / pb.Maximum) * 100).ToString("F2") + "%";
-            }
+                BatteryBar.IsEnabled = true;
+                BatteryBar.Maximum = Convert.ToDouble(report.FullChargeCapacityInMilliwattHours);
+                BatteryBar.Value = Convert.ToDouble(report.RemainingCapacityInMilliwattHours);
 
-            // Add controls to stackpanel
-            sp.Children.Add(txt2);
-            sp.Children.Add(pbLabel);
-            sp.Children.Add(pb);
-            sp.Children.Add(pbPercent);
+                double percentCharged = (BatteryBar.Value / BatteryBar.Maximum) * 100;
+                BatteryBar.Foreground = SetBarColor(percentCharged);
+                BatteryCharge.Text = string.Format("{0:F2}%", percentCharged);
+            }
+        }
+
+        private Brush SetBarColor(double percentCharged)
+        {
+            if (percentCharged > 50)
+            {
+                return new SolidColorBrush(Color.FromArgb(255, 15, 105, 209));
+            }
+            else if (percentCharged > 20)
+            {
+                return new SolidColorBrush(Color.FromArgb(255, 242, 236, 44));
+            }
+            else
+            {
+                return new SolidColorBrush(Color.FromArgb(255, 242, 44, 44));
+            }
         }
 
         async private void AggregateBattery_ReportUpdated(Battery sender, object args)
         {
-            if (reportRequested)
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
-                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                {
-                    RequestAggregateBatteryReport();
-                });
-            }
+                RequestAggregateBatteryReport();
+            });
         }
     }
 }
